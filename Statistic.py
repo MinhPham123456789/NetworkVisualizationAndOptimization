2ca3707f7f89186a2e99df58b74e2cab8e94aa65
MinhPham <jijon745@gmail.com>
2019-10-08 23:37:40 -0400

    Improve statistic

--- Statistic.py
+++ Statistic.py
@@ -3,6 +3,8 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import *
+import collections
+import numpy as np
 
 
 class Statistic(tk.Frame):
@@ -11,9 +13,15 @@ class Statistic(tk.Frame):
         fig = Figure(figsize=(8, 4.5), dpi=200)
         ax = fig.add_subplot(111)
         p = fig.gca()
-        p.hist(stat, 100)
-        p.set_xlabel('{}'.format(attribute), fontsize=15)
-        p.set_ylabel('Frequency', fontsize=15)
+        statistic_dictionary = collections.Counter(x if x else "None" for x in stat)
+        # print(statistic_dictionary.keys())
+        # print(statistic_dictionary.values())
+        # print(statistic_dictionary)
+        y_pos = np.arange(len(statistic_dictionary.keys())) # Arrange bar position
+        p.bar(y_pos, statistic_dictionary.values(), align='center', alpha=0.5)
+        p.set_xticks(y_pos)
+        p.set_xticklabels(statistic_dictionary.keys())
+        p.set_ylabel('Occurrences', fontsize=15)
         canvas = FigureCanvasTkAgg(fig, parent)
         canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
         toolbar = NavigationToolbar2TkAgg(canvas, parent)
